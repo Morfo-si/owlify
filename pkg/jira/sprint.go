f1be68d52a3f6364e2754e06d288a9dcca1e5022
Og B. Maciel <omaciel@ogmaciel.com>
2025-04-18 15:57:06 -0400

    refactor: add JIRA URL constants and improve sprint functionality

--- pkg/jira/sprint.go
+++ pkg/jira/sprint.go
@@ -5,7 +5,12 @@ import (
 	"net/url"
 )
 
-func FetchCurrentSprintIssues(project, component string, sprintNumber int) ([]Issue, error) {
+const (
+	JIRA_URL_BOARD = "rest/agile/1.0/board"
+	JIRA_URL_JQL   = "rest/api/2/search?jql"
+)
+
+func FetchCurrentSprintIssues(project, component string, sprintNumber int, makeGetRequest JiraRequestFunc) ([]Issue, error) {
 	var jql string
 
 	// If no sprint number is provided, fetch issues from all open sprints
@@ -23,7 +28,7 @@ func FetchCurrentSprintIssues(project, component string, sprintNumber int) ([]Is
 		jql += fmt.Sprintf(" AND project = '%s'", project)
 	}
 
-	url := fmt.Sprintf("%s/rest/api/2/search?jql=%s", jiraBaseURL, url.QueryEscape(jql))
+	url := fmt.Sprintf("%s/%s=%s", jiraBaseURL, JIRA_URL_JQL, url.QueryEscape(jql))
 
 	var jiraResponse JiraResponse
 	if err := makeGetRequest(url, &jiraResponse); err != nil {
@@ -33,12 +38,12 @@ func FetchCurrentSprintIssues(project, component string, sprintNumber int) ([]Is
 	return jiraResponse.Issues, nil
 }
 
-func FetchOpenSprints(boardId int) ([]Sprint, error) {
+func FetchOpenSprints(boardId int, makeGetRequest JiraRequestFunc) ([]Sprint, error) {
 	// JQL to find boards for the project and component
 	// For each board, fetch active sprints
 	var allSprints SprintResponse
 
-	sprintURL := fmt.Sprintf("%s/rest/agile/1.0/board/%d/sprint?state=active", jiraBaseURL, boardId)
+	sprintURL := fmt.Sprintf("%s/%s/%d/sprint?state=active", jiraBaseURL, JIRA_URL_BOARD, boardId)
 
 	if err := makeGetRequest(sprintURL, &allSprints); err != nil {
 		return nil, err
